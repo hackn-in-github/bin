042a2d599bd9bbc3b404301ec3ca60a7f7d6a020
tooru <mr.hack.1984+github@gmail.com>
2016-01-07 20:07:29 +0900

    *.synctex.gzも削除するように修正

--- del-auxfiles-2.sh
+++ del-auxfiles-2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for ext
-in .+_e[0-9]+\.pdf$ .+_e[0-9]+\.xbb$ .+_e[0-9]+\.eps$ .+_d[0-9]+\.pl$ .+d[0-9]+\.dta$ .+\.aux$ .+\.log$ .+\.out$ .+\.ans$ .+\.dvi$ .+\.gnuplot$ .+\.table$ .+/texput\.pdf$ .+/texput\.tex$ .+\.dpth
+in .+_e[0-9]+\.pdf$ .+_e[0-9]+\.xbb$ .+_e[0-9]+\.eps$ .+_d[0-9]+\.pl$ .+d[0-9]+\.dta$ .+\.aux$ .+\.log$ .+\.out$ .+\.ans$ .+\.dvi$ .+\.gnuplot$ .+\.table$ .+/texput\.pdf$ .+/texput\.tex$ .+\.synctex\.gz$
 do
 # echo EXT=$ext
  find ./ -regex $ext -exec rm {} \;
