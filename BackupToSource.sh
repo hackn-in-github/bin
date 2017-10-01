e6bbc42c9e15d2e047da39de5ffea3fa4aae0eaa
hackn-in-github <mr.hack.1984+github@gmail.com>
2017-10-01 22:03:03 +0900

    BackupTo(Source|Target)を作成

---
+++ BackupToSource.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#From → To
+#~/tilda /tilda
+#~/TeX /TeX
+#~/Web /Web
+#~/bin /bin
+#~/org /org
+#~/ダウンロード/Github /Github
+#~/ダウンロード/YaTeX /YaTeX
+#~/ダウンロード/emath /emath
+#~/ダウンロード/Photo /Photo
+#~/ダウンロード/アニメ /ビデオ/Anime
+cp -Ppurv ~/tilda /media/tooru/Source/tooru/
+cp -Ppurv ~/TeX /media/tooru/Source/tooru/
+cp -Ppurv ~/Web /media/tooru/Source/tooru/
+cp -Ppurv ~/bin /media/tooru/Source/tooru/
+cp -Ppurv ~/org /media/tooru/Source/tooru/
+cp -Ppurv ~/iijima /media/tooru/Source/tooru/
+cp -Ppurv ~/ダウンロード/Github /media/tooru/Source/tooru/
+cp -Ppurv ~/ダウンロード/YaTeX /media/tooru/Source/tooru/
+cp -Ppurv ~/ダウンロード/emath /media/tooru/Source/tooru/
+cp -Ppurv ~/ダウンロード/Photo /media/tooru/Source/tooru/
+cp -Ppurv ~/ダウンロード/アニメ/20* /media/tooru/Source/tooru/ビデオ/Anime/
---
+++ BackupToTarget.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#From → To
+#~/tilda /tilda
+#~/TeX /TeX
+#~/Web /Web
+#~/bin /bin
+#~/org /org
+#~/ダウンロード/Github /Github
+#~/ダウンロード/YaTeX /YaTeX
+#~/ダウンロード/emath /emath
+#~/ダウンロード/Photo /Photo
+#~/ダウンロード/アニメ /ビデオ/Anime
+cp -Ppurv ~/tilda /media/tooru/Target/tooru/
+cp -Ppurv ~/TeX /media/tooru/Target/tooru/
+cp -Ppurv ~/Web /media/tooru/Target/tooru/
+cp -Ppurv ~/bin /media/tooru/Target/tooru/
+cp -Ppurv ~/org /media/tooru/Target/tooru/
+cp -Ppurv ~/iijima /media/tooru/Target/tooru/
+cp -Ppurv ~/ダウンロード/Github /media/tooru/Target/tooru/
+cp -Ppurv ~/ダウンロード/YaTeX /media/tooru/Target/tooru/
+cp -Ppurv ~/ダウンロード/emath /media/tooru/Target/tooru/
+cp -Ppurv ~/ダウンロード/Photo /media/tooru/Target/tooru/
+cp -Ppurv ~/ダウンロード/アニメ/20* /media/tooru/Target/tooru/ビデオ/Anime/
