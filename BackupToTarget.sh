54b8492f4c5497f1d114360fdd0878dd72c67fe6
hackn-in-github <mr.hack.1984+github@gmail.com>
2018-03-28 16:30:36 +0900

    特殊ディレクトリ名を日本語から英語に変更

--- BackupToTarget.sh
+++ BackupToTarget.sh
@@ -5,19 +5,19 @@
 #~/Web /Web
 #~/bin /bin
 #~/org /org
-#~/ダウンロード/Github /Github
-#~/ダウンロード/YaTeX /YaTeX
-#~/ダウンロード/emath /emath
-#~/ダウンロード/Photo /Photo
-#~/ダウンロード/アニメ /ビデオ/Anime
+#~/Downloads/Github /Github
+#~/Downloads/YaTeX /YaTeX
+#~/Downloads/emath /emath
+#~/Downloads/Photo /Photo
+#~/Downloads/アニメ /ビデオ/Anime
 cp -Ppurv ~/tilda /media/tooru/Target/tooru/
 cp -Ppurv ~/TeX /media/tooru/Target/tooru/
 cp -Ppurv ~/Web /media/tooru/Target/tooru/
 cp -Ppurv ~/bin /media/tooru/Target/tooru/
 cp -Ppurv ~/org /media/tooru/Target/tooru/
 cp -Ppurv ~/iijima /media/tooru/Target/tooru/
-cp -Ppurv ~/ダウンロード/Github /media/tooru/Target/tooru/
-cp -Ppurv ~/ダウンロード/YaTeX /media/tooru/Target/tooru/
-cp -Ppurv ~/ダウンロード/emath /media/tooru/Target/tooru/
-cp -Ppurv ~/ダウンロード/Photo /media/tooru/Target/tooru/
-cp -Ppurv ~/ダウンロード/アニメ/20* /media/tooru/Target/tooru/ビデオ/Anime/
+cp -Ppurv ~/Downloads/Github /media/tooru/Target/tooru/
+cp -Ppurv ~/Downloads/YaTeX /media/tooru/Target/tooru/
+cp -Ppurv ~/Downloads/emath /media/tooru/Target/tooru/
+cp -Ppurv ~/Downloads/Photo /media/tooru/Target/tooru/
+cp -Ppurv ~/Downloads/アニメ/20* /media/tooru/Target/tooru/ビデオ/Anime/
