b77e7c906432ff6ef31b596e7ecfb2157777164b
hackn-in-github <mr.hack.1984+github@gmail.com>
2018-11-14 14:51:59 +0900

    新しいバックアップHDDの追加

--- BackupToTarget.sh
+++ BackupToTarget.sh
@@ -23,3 +23,16 @@ cp -Ppurv ~/Downloads/emath /media/tooru/Target/tooru/
 cp -Ppurv ~/Pictures/Photo /media/tooru/Target/tooru/
 cp -Ppurv ~/Videos/Anime/20* /media/tooru/Target/tooru/ビデオ/Anime/
 cp -Ppurv ~/Videos/AV /media/tooru/Target/tooru/ビデオ/Movie/AV/Videos/
+cp -Ppurv ~/tilda /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Documents/TeX /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Documents/Web /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/bin /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Documents/org /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Documents/iijima /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Documents/emacs /media/tooru/Source 2TB/tooru/
+#cp -Ppurv ~/Downloads/Github /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Downloads/YaTeX /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Downloads/emath /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Pictures/Photo /media/tooru/Source 2TB/tooru/
+cp -Ppurv ~/Videos/Anime/20* /media/tooru/Source 2TB/tooru/ビデオ/Anime/
+cp -Ppurv ~/Videos/AV /media/tooru/Source 2TB/tooru/ビデオ/Movie/AV/Videos/
